d462b98472a902a529a6e05ef71ba068eac291f2
j- <j@skeoh.com>
2020-04-19 18:41:32 +1000

    Add simple instructions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,11 @@ import './App.css';
 const App: React.FC = () => (
   <div className="App container pt-5 pb-5">
     <Game />
+
+    <div className="mt-5 text-center">
+      <h2>How to play</h2>
+      <p>Tap colors to flood-fill the board from the top left corner. The goal is to make the whole board a single color within 25 moves.</p>
+    </div>
   </div>
 );
 
