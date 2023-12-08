6f7877ba8d8a361f06a537df6f5dd4c6b03eb3a1
j- <j@skeoh.com>
2023-12-08 21:56:23 +1100

    Update serialize module and tests

--- src/serialize.test.ts
+++ src/serialize.test.ts
@@ -1,21 +1,33 @@
 import { serialize, deserialize } from './serialize';
 import { Color } from './types';
 
-const cases = [
-  [[], 0, ''],
-  [[5, 4, 5, 4, 5, 0, 1, 2, 3, 4, 5, 5, 1, 2, 3, 4, 5, 5, 4, 5], 20, 'yyu8mz8mzt'],
-] as [Color[], number, string][];
+type Case = [moves: Color[], state: string];
+
+const cases: Case[] = [
+  [[], ''],
+  [[0], '0'],
+  [[1], '7'],
+  [[3, 5], 'x'],
+  [[3, 5, 4], 'xs'],
+  [[5, 3, 4, 1], 'na'],
+  [[5, 3, 4, 1, 2], 'nae'],
+  [[5, 3, 4, 1, 2, 4], 'naq'],
+  [[5, 3, 4, 1, 2, 4, 2, 0], 'naq2'],
+  [[5, 4, 5, 4, 5, 0, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 5], 'tt5drbkyy'],
+  [[5, 4, 1, 3, 1, 5, 1, 5, 0, 5, 1, 0, 2, 4, 5, 3, 1, 0, 2, 1], 'tjvvu1qn18'],
+  [[1, 5, 4, 1, 2, 3, 4, 3, 5, 1, 2, 1, 0, 5, 3, 4, 1, 0, 5, 2, 3, 5, 4, 1], 'vakmb8ur1hxa'],
+];
 
 describe('serialize()', () => {
-  test.each(cases)('serializes %p (%p moves) to %p', (moves, _moveCount, expected) => {
+  test.each(cases)('serializes %p to %p', (moves, state) => {
     const actual = serialize(moves);
-    expect(actual).toBe(expected);
+    expect(actual).toBe(state);
   });
 });
 
 describe('deserialize()', () => {
-  test.each(cases)('deserializes %p (%p moves) from %p', (expected, moveCount, moves) => {
-    const actual = deserialize(moves, moveCount);
-    expect(actual).toEqual(expected);
+  test.each(cases)('deserializes %p from %p', (moves, state) => {
+    const actual = deserialize(state);
+    expect(actual).toEqual(moves);
   });
 });
--- src/serialize.ts
+++ src/serialize.ts
@@ -1,29 +1,45 @@
 import { Color } from './types';
 
+const serializePair = (m0: Color, m1: Color | null): string => {
+  if (m1 == null) m1 = m0;
+  const res = (
+    (m1 * 6) +
+    m0
+  );
+  return res.toString(36);
+};
+
+const deserializePair = (ch: string): [Color, Color | null] => {
+  const n = parseInt(ch, 36);
+  const m1 = Math.floor(n / 6);
+  const m0 = n % 6;
+  return [m0, m0 === m1 ? null : m1];
+};
+
 export const serialize = (moves: Color[]): string => {
-  const colorCount = 6;
   let result = '';
+
   for (let i = 0; i < moves.length; i += 2) {
-    result += (
-      moves[i] * colorCount +
-      (moves[i + 1] || 0)
-    ).toString(36);
+    result += serializePair(
+      moves[i],
+      moves[i + 1] ?? null
+    );
   }
+
   return result;
 };
 
-export const deserialize = (
-  serialized: string,
-  moveCount = serialized.length * 2,
-): Color[] => {
+export const deserialize = (serialized: string): Color[] => {
   const moves: Color[] = [];
-  for (let i = 0; i < serialized.length && i < moveCount; i++) {
+
+  for (let i = 0; i < serialized.length; i++) {
     const ch = serialized.charAt(i);
-    const num = parseInt(ch, 36);
-    moves.push(Math.floor(num / 6));
-    if (i < moveCount - 1) {
-      moves.push(num % 6);
+    const [move0, move1] = deserializePair(ch);
+    moves.push(move0);
+    if (move1 != null) {
+      moves.push(move1);
     }
   }
+
   return moves;
 };
