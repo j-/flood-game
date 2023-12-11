aa1b7745541d15305bea8ed05cc69c3900970f4c
j- <j@skeoh.com>
2023-12-11 16:16:06 +1100

    Indicate current color on buttons

--- src/components/ColorButton.tsx
+++ src/components/ColorButton.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Color, colorNames } from '../types';
+import { CirclePath } from './CirclePath';
 
 export type ColorButtonProps = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'color'> & {
   color: Color;
+  isCurrentColor?: boolean;
 };
 
-export const ColorButton: React.FC<ColorButtonProps> = ({ color, ...props }) => {
+export const ColorButton: React.FC<ColorButtonProps> = ({
+  color,
+  isCurrentColor,
+  ...props
+}) => {
   const name = colorNames.get(color);
   const title = `Flood with ${name?.toLowerCase()}`;
 
@@ -18,6 +24,21 @@ export const ColorButton: React.FC<ColorButtonProps> = ({ color, ...props }) => 
       {...props}
     >
       <span className="sr-only">name</span>
+      {isCurrentColor ? (
+        <svg
+          width={100}
+          height={100}
+          viewBox='0 0 100 100'
+        >
+          <CirclePath
+            outerRadius={.3 * 120}
+            innerRadius={.2 * 120}
+            cx={50}
+            cy={50}
+            fill="#fff"
+          />
+        </svg>
+      ) : null}
     </button>
   );
 };
--- src/components/ColorButtons.tsx
+++ src/components/ColorButtons.tsx
@@ -1,12 +1,14 @@
 import React, { MouseEventHandler, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { flood } from '../store/actions';
 import { Color } from '../types';
 import { ColorButton } from './ColorButton';
 import './ColorButtons.css';
+import { getCurrentColor } from '../store';
 
 const ColorButtons: React.FC = () => {
   const dispatch = useDispatch();
+  const currentColor = useSelector(getCurrentColor);
 
   const handleClickColor = useCallback<MouseEventHandler<HTMLButtonElement>>((e) => {
     const color = parseInt(e.currentTarget.value, 10);
@@ -15,12 +17,12 @@ const ColorButtons: React.FC = () => {
 
   return (
     <div className="ColorButtons d-flex justify-content-between">
-      <ColorButton onClick={handleClickColor} color={Color.RED} />
-      <ColorButton onClick={handleClickColor} color={Color.ORANGE} />
-      <ColorButton onClick={handleClickColor} color={Color.YELLOW} />
-      <ColorButton onClick={handleClickColor} color={Color.GREEN} />
-      <ColorButton onClick={handleClickColor} color={Color.BLUE} />
-      <ColorButton onClick={handleClickColor} color={Color.PURPLE} />
+      <ColorButton onClick={handleClickColor} color={Color.RED} isCurrentColor={currentColor === Color.RED} />
+      <ColorButton onClick={handleClickColor} color={Color.ORANGE} isCurrentColor={currentColor === Color.ORANGE} />
+      <ColorButton onClick={handleClickColor} color={Color.YELLOW} isCurrentColor={currentColor === Color.YELLOW} />
+      <ColorButton onClick={handleClickColor} color={Color.GREEN} isCurrentColor={currentColor === Color.GREEN} />
+      <ColorButton onClick={handleClickColor} color={Color.BLUE} isCurrentColor={currentColor === Color.BLUE} />
+      <ColorButton onClick={handleClickColor} color={Color.PURPLE} isCurrentColor={currentColor === Color.PURPLE} />
     </div>
   );
 };
